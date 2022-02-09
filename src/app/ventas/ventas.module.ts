073bf81fd3a2d744e8ee958adc694dd2356da14b
angularsantanderi <angularsantanderi@gmail.com>
2022-02-09 12:29:14 +0100

    add params

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { VentasRoutingModule } from './ventas-routing.module';
 import { InicioVentasComponent } from './inicio-ventas/inicio-ventas.component';
 import { TablaClientesComponent } from './clientes/tabla-clientes/tabla-clientes.component';
+import { VisualizarClienteComponent } from './clientes/visualizar-cliente/visualizar-cliente.component';
 
 
 @NgModule({
   declarations: [
     InicioVentasComponent,
-    TablaClientesComponent
+    TablaClientesComponent,
+    VisualizarClienteComponent
   ],
   imports: [
     CommonModule,
