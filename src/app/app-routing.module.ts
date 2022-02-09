9c412ec7c9aa0c8554f9f7ac767702abfcd4bea6
angularsantanderi <angularsantanderi@gmail.com>
2022-02-09 10:56:01 +0100

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SoporteComponent } from './soporte/soporte.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'soporte', component: SoporteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
